0088857863c153823812d69c8515ddb875ff1879
Deepanshu srivastav <39976215+deepanshusrivastav@users.noreply.github.com>
2023-08-24 16:40:37 +0530

    Create Android2.java

---
+++ src/main/java/Android2.java
@@ -0,0 +1,78 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.util.List;
+
+public class AndroidApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "username" : System.getenv("LT_USERNAME"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void AndroidApp1(String device, String version, String platform) {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build","Java TestNG Android");
+            capabilities.setCapability("name",platform+" "+device+" "+version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion",version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", false);
+            capabilities.setCapability("visual", true);  
+            capabilities.setCapability("devicelog", true);
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            driver.get("https://www.nike.com");
+            Thread.sleep(5000);
+
+            driver.get("https://www.flipkart.com");
+            Thread.sleep(5000);
+
+            driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div")).click();
+            Thread.sleep(3000);
+ 
+
+            driver.get("https://www.tesla.com");
+            Thread.sleep(5000);
+
+
+            driver.get("https://www.lambdatest.com");
+            Thread.sleep(5000);
+
+
+            driver.get("https://www.lambdatest.com/pricing");
+            Thread.sleep(5000);
+
+            driver.quit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                driver.quit();
+            }catch(Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
